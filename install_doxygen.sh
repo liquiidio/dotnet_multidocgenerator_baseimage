4d1b33f1c04f2b5fda9fb9ace76efff67ebfa69a
corvinmadh <grizzlycorv@googlemail.com>
2022-05-05 01:14:07 +0000

    Update install_doxygen.sh

--- install_doxygen.sh
+++ install_doxygen.sh
@@ -1,1 +1,18 @@
-apt-get update && apt-get install -y software-properties-common && apt-add-repository universe && apt-get update && apt-get install -y doxygen
+apt-get update && apt-get install -y software-properties-common && apt-add-repository universe #&& apt-get update && apt-get install -y doxygen
+
+#download doxygen
+git clone https://github.com/doxygen/doxygen.git
+cd doxygen
+
+#build doxygen
+mkdir build
+cd build
+cmake -G "Unix Makefiles" ..
+make
+
+# install doxygen
+make install
+
+cd ../..
+
+npm install gitbook-cli -g
